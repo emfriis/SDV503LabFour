d8cfe1a3ce78c33be8da00b0bd73f82d9784124b
Emily Friis <emily-friis@live.nmit.ac.nz>
2020-04-06 13:26:12 +1200

    changed a variable name

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ function binaryEasy(numConvertFirst) {  // Uses the toString method to convert a
 console.log(binaryEasy(10))
 
 function binarySome(numConvertSecond) { // Uses a for loop to convert a decimal number to a representative binary string by appending to an empty string, so long as the decimal is less that 2 ^ 32.
-    var numBinary = "";
+    var numBinaryFirst = "";
     if (numConvertSecond == 0) {
-        numBinary = "0";
-        return numBinary;
+        numBinaryFirst = "0";
+        return numBinaryFirst;
     }
     for (var i = 32; i > -1; i--) {
         if (numConvertSecond >= (2 ** i)) {
             numConvertSecond -= 2 ** i;
-            numBinary += "1";
-        } else if (numBinary != "") {
-            numBinary += "0";
+            numBinaryFirst += "1";
+        } else if (numBinaryFirst != "") {
+            numBinaryFirst += "0";
         }
     }
-    return numBinary;
+    return numBinaryFirst;
 }
 
 console.log(binarySome(10));
